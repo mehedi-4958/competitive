0b28106544b97de38872790365a731550b3ecc36
Mehedi <babu.oshi@gmail.com>
2021-11-16 01:57:18 +0600

    bee1101 trying

---
+++ uri_1101-sequence-of-numbers-and-sum/bin/uri_1101_sequence_of_numbers_and_sum.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'dart:math';
+
+void main() {
+  int sum = 0;
+  for(int i = 0;;i++){
+    String s = stdin.readLineSync()!;
+    List l = s.split(' ');
+
+    if(l[0] == 0 || l[1] == 0){
+      break;
+    }
+  }
+}
